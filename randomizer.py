37375cf96e602d0f18c5b81a20e10c1ce0ae79cd
amarkouskeee <alexmarkouski@gmail.com>
2021-12-29 14:50:35 +0300

    FINISHED🏁🏁🏁🏁🏁🏁

--- randomizer.py
+++ randomizer.py
@@ -2,27 +2,26 @@ from tkinter import *
 from tkinter import Tk
 
 
-checkmates = {1:'penis',
 
-               2: ' boop',
-               3:'cheee'}
+#checkmates =
 
 
 root = Tk()
 root.title('RandSpot')
 
 
+
 root.geometry('800x520')
 #bg_photo = PhotoImage(file='(G:\w11.jpg)')
 #root_bg  = Label(root, bg=)
 photo = ()
-root.config(bg='photo')
+root.config(bg='#333333')
 
 frame_b = Frame()
 frame_a = Frame()
 
-label_a = Label(master=frame_a, text='Розыгрыш Iphone 13')
-label_b = Label(master=frame_b, text=f'Всего участников: 11 957')
+label_a = Label(master=frame_a, text='Розыгрыш Iphone 13',)
+label_b = Label(master=frame_b, text=f'Всего участников: 12091')
 label_b.pack()
 label_a.pack()
 
@@ -47,8 +46,8 @@ btn.pack(pady=5,padx=3)
 
 
 def getting_text(event):
-    res = 'wh.r.uu'
-    textBox.delete('1.0', END)
+    res = 'vehanx'
+    textBox.delete('1.0')
     lab['text'] = res
 
 
